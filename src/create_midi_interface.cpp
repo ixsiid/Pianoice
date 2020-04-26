4bdbe7f35853505bb027c4d574cc746d5324eb18
ixsiid <ixsiid@halzion.net>
2020-04-26 07:28:00 +0100

    first commit

---
+++ src/create_midi_interface.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "midi.hpp"
+
+int main () {
+    MidiInterface * p = new MidiInterface("foobar");
+    p->connect("Midi Through", MidiDirection::OUT);
+    while (true) {
+         std::cin.get();
+    }
+    exit(0);
+}
+
+
+
+
+
